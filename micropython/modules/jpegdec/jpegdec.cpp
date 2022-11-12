f26448c13bb315e93fb59dcbd3f804027874105f
iestyn lloyd <iestyn@gmail.com>
2022-11-12 21:02:28 +0000

    test

--- micropython/modules/jpegdec/jpegdec.cpp
+++ micropython/modules/jpegdec/jpegdec.cpp
@@ -133,7 +133,7 @@ MICROPY_EVENT_POLL_HOOK
                 current_graphics->pixel({pDraw->x + x, pDraw->y + y});
                 
             }
-            mp_raise_msg(&mp_type_RuntimeError, "yez - %i\n",y);
+            mp_raise_msg(&mp_type_RuntimeError, "yez y= %d \n", y);
         }
     } else {
         for(int y = 0; y < pDraw->iHeight; y++) {
